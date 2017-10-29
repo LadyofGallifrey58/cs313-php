1361b8d371613bd8dec58fdfd9e0cbdfbb8ce057
Sabrina Bell <skibri@yahoo.com>
2017-10-28 17:32:12 -0700

    log in set up

--- bookStore.php
+++ bookStore.php
@@ -1,5 +1,8 @@
 <?php
 
+session_start();
+
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -43,22 +46,91 @@ catch (PDOException $ex)
    <link rel="stylesheet" href="bookStore.css">
 
 <body>
-   <form action="" method="post">
-   <div class="btn-group">
-      <button type="submit" class="btn" id="choice" name="choice" value="author_id">Browse By</button>
-      <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         <span class="sr-only">Toggle Dropdown</span>
-      </button>
-      <div class="dropdown-menu">
-         <h6 class="dropdown-header">Browse by:</h6>
-         <button class="dropdown-item browse" type="button" name="browse" value="author_id">Author</button>
-         <button class="dropdown-item browse" type="button" name="browse" value="book_name">Title</button>
-         <button class="dropdown-item browse" type="button" name="browse" value="release_date">Release Date</button>
-         <button class="dropdown-item browse" type="button" name="browse" value="price">Price</button> 
-         <!— reverse order? —>
+
+<?php
+if ((!isset($_SESSION['userEmail']) && !isset($_SESSION['userPassword'])) && (isset($_POST['userEmail']) && isset($_POST['userPassword'])))
+{
+   $_SESSION['userEmail'] = filter_var($_POST['userEmail'], FILTER_SANITIZE_EMAIL);
+   $_SESSION['userPassword'] = filter_var($_POST['userPassword'], FILTER_SANITIZE_STRING);
+
+   $passwordHash = password_hash($_SESSION['userPassword'], PASSWORD_DEFAULT);
+   $statement = $db->prepare('SELECT passwords.password, accounts.first_name, accounts.last_name FROM accounts, passwords WHERE accounts.password = passwords.password_id AND accounts.email=:email');
+   $statement->bindValue(':email', $_SESSION['userEmail'], PDO::PARAM_STR);
+   $result;
+
+   $statement->execute();
+   $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+   if (password_verify($result['password'], $passwordHash))
+   {
+      $_SESSION['first_name'] = $result['first_name'];
+      $_SESSION['last_name'] = $result['last_name'];
+   }
+}
+?>
+
+   <div id="header">
+      <?php
+      if (!(isset($_SESSION['first_name']) && isset($_SESSION['last_name'])))
+      { ?>
+         <div class="dropdown headerButton">
+            <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">My Account</button>
+            <div class="dropdown-menu">
+               <form class="px-4 py-3">
+                  <div class="form-group">
+                     <label for="userEmail">Email address</label>
+                     <input type="email" class="form-control" id="userEmail" name="userEmail" placeholder="email@example.com">
+                  </div>
+                  <div class="form-group">
+                     <label for="userPassword">Password</label>
+                     <input type="password" class="form-control" id="userPassword" name="userPassword" placeholder="Password">
+                  </div>
+                  <div class="form-check">
+                     <label class="form-check-label">
+                     <input type="checkbox" class="form-check-input">
+                     Remember me
+                     </label>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Sign in</button>
+               </form>
+            <div class="dropdown-divider"></div>
+               <a class="dropdown-item" href="#">Sign up</a>
+               <a class="dropdown-item" href="#">Forgot password?</a>
+            </div>
+         </div>
+      <?php } 
+      else { ?>
+         <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               My Account
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+               <h4><?php echo "Welcome, " . $_SESSION['first_name']?></h4>
+               <div class="dropdown-divider"></div>
+               <a class="dropdown-item" href="#">Account Info</a>
+               <a class="dropdown-item" href="#">My Cart</a>
+               <a class="dropdown-item" href="#"><?php echo "Not " . $_SESSION['first_name'] . "? Sign Out" ?></a>
+            </div>
+         </div>
+<?php } ?>
+           
+      <form action="" method="post" class="headerButton">
+      <div class="btn-group">
+         <button type="submit" class="btn" id="choice" name="choice" value="author_id">Browse By</button>
+         <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="sr-only">Toggle Dropdown</span>
+         </button>
+         <div class="dropdown-menu dropdown-menu-right">
+            <h6 class="dropdown-header">Browse by:</h6>
+            <button class="dropdown-item browse" type="button" name="browse" value="author_id">Author</button>
+            <button class="dropdown-item browse" type="button" name="browse" value="book_name">Title</button>
+            <button class="dropdown-item browse" type="button" name="browse" value="release_date">Release Date</button>
+            <button class="dropdown-item browse" type="button" name="browse" value="price">Price</button> 
+            <!— reverse order? —>
+         </div>
       </div>
+      </form>
    </div>
-   </form>
 
 
    <?php
@@ -66,8 +138,7 @@ catch (PDOException $ex)
    {
       if ($_POST['choice'] == 'author_id')
       {
-         $sort = '(authors.last_name, authors.first_name, authors.middle_name)'; //CHECK HERE
-         echo 'Made it2!';
+         $sort = '(authors.last_name, authors.first_name, authors.middle_name)';
       }
       else
       {
@@ -132,6 +203,11 @@ catch (PDOException $ex)
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
    <script src="bookStore.js"></script>
+
+  <!—for now —>
+   <?php
+   session_unset();
+   session_destroy(); ?>
 </body>
 </html>
 
